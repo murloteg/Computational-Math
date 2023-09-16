6b139544a562bb04a00543f868feeea07431f4cf
Kirill Bolotov <111644292+murloteg@users.noreply.github.com>
2023-09-17 00:25:45 +0700

    Исправлена логика в методе findRootByDichotomyMethod. Улучшен формат логов (#2)

--- lab1/src/main/java/ru/nsu/bolotov/service/CubicEquationSolver.java
+++ lab1/src/main/java/ru/nsu/bolotov/service/CubicEquationSolver.java
@@ -10,6 +10,8 @@ public final class CubicEquationSolver {
 
     public static void solveEquation(InputDataRepresentation inputData) {
         CubicEquationParameters cubicEquationParameters = inputData.cubicEquationParameters();
+        LOGGER.trace("[INPUT: x^3 + ({} * x^2) + ({} * x) + {}]", cubicEquationParameters.parameterA(),
+                cubicEquationParameters.parameterB(), cubicEquationParameters.parameterC());
         QuadraticEquationParameters quadraticEquationParameters = new QuadraticEquationParameters(
                 3,
                 2 * cubicEquationParameters.parameterA(),
@@ -29,8 +31,8 @@ public final class CubicEquationSolver {
     ) {
         CubicEquationParameters cubicEquationParameters = inputData.cubicEquationParameters();
         double epsilon = inputData.calculationErrorParameters().epsilon();
-        double minRoot = quadraticEquationSolution.firstRoot().orElseThrow(IllegalStateException::new);
-        double maxRoot = quadraticEquationSolution.secondRoot().orElseThrow(IllegalStateException::new);
+        double minRoot = quadraticEquationSolution.firstRoot().orElseThrow(IllegalArgumentException::new);
+        double maxRoot = quadraticEquationSolution.secondRoot().orElseThrow(IllegalArgumentException::new);
         if (getFunctionValue(cubicEquationParameters, minRoot) > epsilon &&
                 getFunctionValue(cubicEquationParameters, maxRoot) > epsilon) {
             SegmentRepresentation segment = findRootLocationOnTheLeftSegment(inputData, minRoot);
@@ -41,12 +43,14 @@ public final class CubicEquationSolver {
             findRootByDichotomyMethod(inputData, segment);
         } else if (getFunctionValue(cubicEquationParameters, minRoot) > epsilon &&
                 Math.abs(getFunctionValue(cubicEquationParameters, maxRoot)) < epsilon) {
-            LOGGER.info("The root was found! [Root: {}; Multiplicity: {}]", maxRoot, 2);
+            LOGGER.info("The root was found! [Root: {}; Multiplicity: {}]; [Function value: |f(x)| = {}]",
+                    maxRoot, 2, getFunctionValue(cubicEquationParameters, maxRoot));
             SegmentRepresentation segment = findRootLocationOnTheLeftSegment(inputData, minRoot);
             findRootByDichotomyMethod(inputData, segment);
         } else if (Math.abs(getFunctionValue(cubicEquationParameters, minRoot)) < epsilon &&
                 getFunctionValue(cubicEquationParameters, maxRoot) < (-1) * epsilon) {
-            LOGGER.info("The root was found! [Root: {}; Multiplicity: {}]", minRoot, 2);
+            LOGGER.info("The root was found! [Root: {}; Multiplicity: {}]; [Function value: |f(x)| = {}]",
+                    minRoot, 2, getFunctionValue(cubicEquationParameters, minRoot));
             SegmentRepresentation segment = findRootLocationOnTheRightSegment(inputData, maxRoot);
             findRootByDichotomyMethod(inputData, segment);
         } else if (getFunctionValue(cubicEquationParameters, minRoot) > epsilon &&
@@ -68,7 +72,7 @@ public final class CubicEquationSolver {
         CubicEquationParameters cubicEquationParameters = inputData.cubicEquationParameters();
         double epsilon = inputData.calculationErrorParameters().epsilon();
         if (Math.abs(getFunctionValue(cubicEquationParameters, 0)) < epsilon) {
-            LOGGER.info("The only one root was found! [Root: {}; Multiplicity: {}]", 0, 3);
+            LOGGER.info("The root was found! [Root: {}; Multiplicity: {}]", 0, 3);
         } else if (getFunctionValue(cubicEquationParameters, 0) < (-1) * epsilon) {
             SegmentRepresentation segment = findRootLocationOnTheRightSegment(inputData, 0);
             findRootByDichotomyMethod(inputData, segment);
@@ -109,15 +113,22 @@ public final class CubicEquationSolver {
     private static void findRootByDichotomyMethod(InputDataRepresentation inputData, SegmentRepresentation segment) {
         CubicEquationParameters cubicEquationParameters = inputData.cubicEquationParameters();
         double epsilon = inputData.calculationErrorParameters().epsilon();
-        double middle = (segment.getLeftBorder() + segment.getRightBorder()) / 2;
-        if (Math.abs(getFunctionValue(cubicEquationParameters, middle)) < epsilon) {
-            LOGGER.info("The root was found! [Root: {}]", middle);
-        } else if (getFunctionValue(cubicEquationParameters, middle) > epsilon) {
-            segment.setRightBorder(middle);
-            findRootByDichotomyMethod(inputData, segment);
-        } else if (getFunctionValue(cubicEquationParameters, middle) < (-1) * epsilon) {
-            segment.setLeftBorder(middle);
-            findRootByDichotomyMethod(inputData, segment);
+        double leftBorder = segment.getLeftBorder();
+        double rightBorder = segment.getRightBorder();
+        boolean isCaseWhenLeftExtremumUnderAxis = getFunctionValue(cubicEquationParameters, leftBorder) < 0;
+        while (true) {
+            double middle = (leftBorder + rightBorder) / 2;
+            double functionValue = getFunctionValue(cubicEquationParameters, middle);
+            if (Math.abs(functionValue) < epsilon) {
+                LOGGER.info("The root was found! [Root: {}]; [Function value: |f(x)| = {}]", middle, functionValue);
+                break;
+            } else if (functionValue > epsilon) {
+                leftBorder = isCaseWhenLeftExtremumUnderAxis ? leftBorder : middle;
+                rightBorder = isCaseWhenLeftExtremumUnderAxis ? middle : rightBorder;
+            } else if (functionValue < (-1) * epsilon) {
+                leftBorder = isCaseWhenLeftExtremumUnderAxis ? middle : leftBorder;
+                rightBorder = isCaseWhenLeftExtremumUnderAxis ? rightBorder : middle;
+            }
         }
     }
 
